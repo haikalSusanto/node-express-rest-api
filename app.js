15fc953a3d5f84ab4e963014db5e50b685ba9f25
haikalSusanto <haikalsusanto21@gmail.com>
2021-06-15 00:24:11 +0700

    Established connection to MongoDB (hardcoded)

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const mongoose = require('mongoose');
+
 app.get('/', (req, res) => {
     res.send('Success');
 });
 
+
+mongoose.connect(
+    'mongodb+srv://express_rest_acc:VcffflsQy80F4ejP@express-rest.bzb5z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    { 
+        seNewUrlParser: true,
+        useUnifiedTopology: true
+    }, 
+    () => {console.log('Connected to MongoDB')})
 
 app.listen(port, () => {
     console.log(`app listening at ${port}`)
